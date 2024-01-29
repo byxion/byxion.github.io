d61b00a83021538c5fa28d2de3e1663397f84821
mateo <mateo.guenot2003@gmail.com>
2024-01-29 16:52:52 +0100

    classique

--- js/app.js
+++ js/app.js
@@ -28,25 +28,6 @@ const showPhones = () => {
 
 document.addEventListener("DOMContentLoaded", showPhones)
 
-// Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
-
-function showInstallPromotion() {
-
-}
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    showInstallPromotion();
-    // Optionally, send analytics event that PWA install promo was shown.
-    console.log(`'beforeinstallprompt' event was fired.`);
-});
-
-
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
